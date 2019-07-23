264cbaff5812542d6a00b3bddb89bf00669a100d
Anderson <anderson.dias.sampaio@gmail.com>
2019-07-23 19:32:27 -0300

    In class Operaction: created attributes and methods

--- app/src/main/java/com/example/calculadora/Operaction.java
+++ app/src/main/java/com/example/calculadora/Operaction.java
@@ -1,5 +1,51 @@
 package com.example.calculadora;
 
 public class Operaction {
+    private float a=0, b=0, sum, subtraction, division, multiplication;
 
+    public Operaction(float c, float g) {
+        this.a = c;
+        this.b = g;
+    }
+
+    public float getA() {
+        return a;
+    }
+
+    public float getB() {
+        return b;
+    }
+
+    public float getSum() {
+        return sum;
+    }
+
+    public void setSum() {
+        this.sum = this.a+this.b;
+    }
+
+    public float getSubtraction() {
+        return subtraction;
+    }
+
+    public void setSubtraction() {
+        this.subtraction = this.a-this.b;
+    }
+
+    public float getDivision() {
+        return division;
+    }
+
+    public void setDivision(){
+
+        this.division = this.a/this.b;
+    }
+
+    public float getMultiplicacao() {
+        return multiplication;
+    }
+
+    public void setMultiplicacao() {
+        this.multiplication = this.a*this.b;
+    }
 }
