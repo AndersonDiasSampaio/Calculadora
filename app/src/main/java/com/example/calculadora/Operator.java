025423cb738fda6b4222ff449342d9e81b9e571d
Anderson <anderson.dias.sampaio@gmail.com>
2019-07-23 19:18:58 -0300

    Operator class has create

---
+++ app/src/main/java/com/example/calculadora/Operator.java
@@ -0,0 +1,8 @@
+package com.example.calculadora;
+import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.ParseException;
+public class Operator implements Serializable {
+
+
+}
